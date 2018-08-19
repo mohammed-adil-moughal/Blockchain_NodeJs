4cecf740828832b6fc4b387f4eb6d5fae18f4bd7
mohammed-adil-moughal <mohammedadil755@gmail.com>
2018-08-19 11:48:28 +0300

    added websockets

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
+const P2pserver = require('./p2p-server');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pserver = new P2pserver(bc);
 
 app.use(bodyParser.json())
 
@@ -23,3 +25,7 @@ app.post('/mine',(req,res)=>{
 app.listen(HTTP_PORT,()=>{
    console.log(`Listening on port ${HTTP_PORT}`)
 });
+
+p2pserver.listen();
+// HTTP_PORT=3002 P2P_PORT=5002 PEERS=ws://localhost:5001 npm run dev
+// HTTP_PORT=3003 P2P_PORT=5003 PEERS=ws://localhost:5001,ws://localhost:5002  npm run dev
