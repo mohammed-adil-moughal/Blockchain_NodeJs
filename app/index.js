e5fa19f5f37dffa3353f1e42a6d332cfbd831d53
mohammed-adil-moughal <mohammedadil755@gmail.com>
2018-08-19 12:24:14 +0300

    added sync chain to all connectes sockets

--- app/index.js
+++ app/index.js
@@ -18,7 +18,8 @@ app.get('/blocks',(req,res)=>{
 app.post('/mine',(req,res)=>{
   const block =bc.addBlock(req.body.data);
   console.log(`new block added ${block.toString}`);
-
+  
+  p2pserver.syncChains();
   res.redirect('/blocks');
 });
 
