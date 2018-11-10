c75b1f62cea639bfba65dfed11e456052c831ad9
mohammed-adil-moughal <mohammedadil755@gmail.com>
2018-11-10 21:24:52 +0300

    added wallet transactions

--- wallet/index.js
+++ wallet/index.js
@@ -1,5 +1,6 @@
 const ChainUtil = require('../chain-util');
-const { INITIAL_BALANCE } = require('../config')
+const Transaction=require('./transaction');
+const { INITIAL_BALANCE } = require('../config');
 class Wallet{
     constructor(){
         this.balance= INITIAL_BALANCE;
@@ -15,6 +16,24 @@ class Wallet{
 
     sign(dataHash){
         return this.keyPair.sign(dataHash);
+    }
+
+    createTransaction(recipient,amount,transactionPool){
+        if(amount > this.balance){
+            console.log(`Amount :${amount} exceeds current balance:${this.balance}`);
+            return;
+        }
+
+        let transaction= transactionPool.existingTransaction(this.publicKey);
+        
+        if(transaction){
+            transaction.update(this,recipient,amount);
+        } else {
+            transaction = Transaction.newTransaction(this,recipient,amount);
+            transactionPool.updateOrAddTransaction(transaction);
+        }
+
+        return transaction;   
     }
 }
 
