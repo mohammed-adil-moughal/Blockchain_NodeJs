bc032bddabd789a7c0b72cfd31e82c8ae59a29eb
mohammed-adil-moughal <mohammedadil755@gmail.com>
2018-11-10 21:50:49 +0300

    added formatting

--- wallet/index.test.js
+++ wallet/index.test.js
@@ -19,19 +19,19 @@ describe('Wallet', () => {
             transaction = wallet.createTransaction(recipient, sendAmount, tp);
         });
 
-        describe('and doing the same transaction',()=>{
-            beforeEach(()=>{
-                wallet.createTransaction(recipient,sendAmount,tp);
+        describe('and doing the same transaction', () => {
+            beforeEach(() => {
+                wallet.createTransaction(recipient, sendAmount, tp);
             });
 
-            it('doubles the `sendAmount` substracted from the wallet balance',()=>{
+            it('doubles the `sendAmount` substracted from the wallet balance', () => {
                 expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
-                .toEqual(wallet.balance -sendAmount *2);
+                    .toEqual(wallet.balance - sendAmount * 2);
             });
 
-            it('clones the `sendAmount` output for the recipient',()=>{
-                expect(transaction.outputs.filter(output =>output.address === recipient)
-                .map(output => output.amount)).toEqual([sendAmount,sendAmount])
+            it('clones the `sendAmount` output for the recipient', () => {
+                expect(transaction.outputs.filter(output => output.address === recipient)
+                    .map(output => output.amount)).toEqual([sendAmount, sendAmount])
             });
         });
 
