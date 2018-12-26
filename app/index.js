a341d141ce39a863c91f0f254aa60e69142571cb
mohammed-adil-moughal <mohammedadil755@gmail.com>
2018-12-26 12:40:59 +0300

    added get and post endpoints

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pserver = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pserver = new P2pserver(bc);
 
 app.use(bodyParser.json())
@@ -22,6 +26,18 @@ app.post('/mine',(req,res)=>{
   p2pserver.syncChains();
   res.redirect('/blocks');
 });
+
+app.get('/transactions',(req,res) => {
+  res.json(tp.transactions)
+});
+
+app.post('/transact',(req, res) => {
+ const { recepient, amount } =req.body;
+ const transaction = wallet.createTransaction(recepient,amount,tp);
+
+ res.redirect('/transactions');
+});
+
 
 app.listen(HTTP_PORT,()=>{
    console.log(`Listening on port ${HTTP_PORT}`)
