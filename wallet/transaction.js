41f03e1c28efcb8ff2b21e8b969399b18e7d0835
mohammed-adil-moughal <mohammedadil755@gmail.com>
2018-12-27 12:12:33 +0300

    aded mining transaction logic

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require("../chain-util");
+const { MINING_REWARD } = require('../config')
 
 class Transaction {
   constructor() {
@@ -28,29 +29,38 @@ class Transaction {
     return this;
   }
 
-  static newTransaction(senderWallet, recepient, amount) {
+  static transactionWithOutputs(senderWallet,outputs){
     const transaction = new this();
+    transaction.outputs.push(...outputs);
+    Transaction.signTransaction(transaction, senderWallet);
 
+    return transaction
+  }
+
+  static newTransaction(senderWallet, recepient, amount) {
     if (amount > senderWallet.balance) {
       console.log(`Amount :${amount} exceed balance`);
       return;
     }
 
-    transaction.outputs.push(
-      ...[
-        {
-          amount: senderWallet.balance - amount,
-          address: senderWallet.publicKey
-        },
-        {
-          amount,
-          address: recepient
-        }
-      ]
-    );
-    Transaction.signTransaction(transaction, senderWallet);
+    return Transaction.transactionWithOutputs(senderWallet, [
+      {
+        amount: senderWallet.balance - amount,
+        address: senderWallet.publicKey
+      },
+      {
+        amount,
+        address: recepient
+      }
+    ])
 
-    return transaction;
+  }
+  
+  static rewardTransaction(minerWallet,blockchainWallet){
+    return Transaction.transactionWithOutputs(blockchainWallet,[{
+      amount:MINING_REWARD,
+      address:minerWallet.publicKey
+    }])
   }
 
   static signTransaction(transaction, senderWallet) {
